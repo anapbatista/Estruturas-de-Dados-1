93d8401f2c1e117234b6718e4706ece15941926f
anapbatista <anapbatista@usp.br>
2024-11-26 09:51:59 -0300

    implementação AVL

--- TAD Arvore binaria de busca/arvoredebusca.c
+++ TAD Arvore binaria de busca/arvoredebusca.c
@@ -106,3 +106,178 @@ int remover(no **p, elem *x)
         }
     }
 }
+
+// AVL - Arvore de busca balanceada
+void rotacao_dd(no **r)
+{ // rotacao simples p/esquerda
+
+    no *pai = *r;
+    no *filho = pai->dir;
+
+    pai->dir = filho->esq;
+    filho->esq = pai;
+    pai->fb = 0;
+    filho->fb = 0;
+    *r = filho;
+}
+
+void rotacao_ee(no **r)
+{ // rotacao simples p/direita
+
+    no *pai = *r;
+    no *filho = pai->esq;
+    pai->esq = filho->dir;
+    filho->dir = pai;
+    pai->fb = 0;
+    filho->fb = 0;
+    *r = filho;
+}
+
+void rotacao_ed(no **r)
+{ // rotacao dupla esquerda-direita
+
+    no *pai = *r;
+    no *filho = pai->esq;
+    no *neto = filho->dir;
+    pai->esq = neto->dir;
+    filho->dir = neto->esq;
+    neto->esq = filho;
+    neto->dir = pai;
+
+    switch (neto->fb)
+    {
+    case -1:
+        pai->fb = 1;
+        filho->fb = 0;
+        break;
+    case 0:
+        pai->fb = 0;
+        filho->fb = 0;
+        break;
+    case 1:
+        pai->fb = 0;
+        filho->fb = -1;
+        break;
+    }
+    neto->fb = 0;
+    *r = neto;
+}
+
+void rotacao_de(no **r)
+{ // rotação dupla direita-esquerda
+    no *pai = *r;
+    no *filho = pai->dir;
+    no *neto = filho->esq;
+    pai->dir = neto->esq;
+    filho->esq = neto->dir;
+    neto->dir = filho;
+    neto->esq = pai;
+
+    switch (neto->fb)
+    {
+    case -1:
+        pai->fb = 1;
+        filho->fb = 0;
+        break;
+    case 0:
+        pai->fb = 0;
+        filho->fb = 0;
+        break;
+    case 1:
+        pai->fb = 0;
+        filho->fb = -1;
+        break;
+    }
+    neto->fb = 0;
+    *r = neto;
+}
+
+int inserir_avl(no **p, elem *x)
+{
+    int cresceu;
+    return (aux_inserir_avl(p, x, &cresceu));
+}
+
+int aux_inserir_avl(no **p, elem *x, int *cresceu)
+{
+    if (*p == NULL)
+    {
+        *p = (no *)malloc(sizeof(no));
+        (*p)->info = *x;
+        (*p)->fb = 0;
+        (*p)->esq = (*p)->dir = NULL;
+        *cresceu = 1;
+        return (1);
+    }
+    else if ((*p)->info == *x)
+    {
+        return 0;
+    }
+    else if (*x < (*p)->info)
+    {
+        if (aux_inserir_avl(&(*p)->esq, x, cresceu))
+        {
+            if (*cresceu)
+            {
+                switch ((*p)->fb)
+                {
+                case -1:
+                    if ((*p)->esq->fb == -1)
+                    {
+                        rotacao_ee(p);
+                    }
+                    else
+                    {
+                        rotacao_ed(p);
+                    }
+                    *cresceu = 0;
+                    break;
+                case 0:
+                    (*p)->fb = -1;
+                    *cresceu = 1;
+                    break;
+                case 1:
+                    (*p)->fb = 0;
+                    *cresceu = 0;
+                    break;
+                }
+            }
+            return 1;
+        }
+        return 0;
+    }
+    else if (*x > (*p)->info)
+    {
+        if (aux_inserir_avl(&(*p)->dir, x, cresceu))
+        {
+            if (*cresceu)
+            {
+                switch ((*p)->fb)
+                {
+                case -1:
+                    (*p)->fb = 0;
+                    *cresceu = 0;
+                    break;
+                case 0:
+                    (*p)->fb = 1;
+                    *cresceu = 1;
+                    break;
+                case 1:
+                    if ((*p)->dir->fb == 1)
+                    {
+                        rotacao_dd(p);
+                    }
+                    else
+                    {
+                        rotacao_de(p);
+                    }
+                    *cresceu=0;
+                    break;
+                }
+        
+            }
+        return 1;
+        }
+    return 0;
+    }
+}
--- TAD Arvore binaria de busca/arvoredebusca.h
+++ TAD Arvore binaria de busca/arvoredebusca.h
@@ -1,20 +1,29 @@
-//arvore binaria de busca ou ordenada
+// arvore binaria de busca ou ordenada
 
 typedef int elem;
-typedef struct bloco{
+typedef struct bloco
+{
     elem info;
     struct bloco *esq, *dir;
-}no;
+    int fb; // AVL
+} no;
 
-typedef struct{
+typedef struct
+{
     no *raiz;
-} arvore;   
+} arvore;
 
 arvore *create(void);
 int isempty(arvore *a);
 int buscar(no *p, elem *x);
-int inserir(no**p, elem *x);
+int inserir(no **p, elem *x);
 elem busca_maior(no **p);
 int remover(no **p, elem *x);
 
- 
+// arvore de busca balanceada (AVL)
+void rotacao_dd(no **r);
+void rotacao_ee(no **r);
+void rotacao_ed(no **r);
+void rotacao_de(no **r);
+int inserir_avl(no**p,elem*x);
+int aux_inserir_avl(no**p, elem *x, int*cresceu);
